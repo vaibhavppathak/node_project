dd412ad2464405fde9bedb3b8a523876d4497e7f
saurabhketech <shekhar>
2017-02-21 12:08:43 +0530

    getting user data from mongo_id

---
+++ mongodb/db.js
@@ -0,0 +1,24 @@
+// the middleware function
+module.exports = function() {
+    var mongoose = require('mongoose'); //require mongoose module
+    var conn = mongoose.connect('mongodb://127.0.0.1/Project'); //connection to mongodb
+    
+    // create schema 
+    var usercreate = mongoose.Schema({
+        username:{type:String,required:true,index:{unique:true}},
+        password:{ type: String, required: true },
+        email : {type:String,required:true,index:{unique:true}},
+        firstname:{ type: String, required: true },
+        lastname: { type: String, required: true },
+    }, {
+        strict: true,
+        collection: 'users'
+    });
+ 
+    var usercreate = conn.model('create', usercreate);
+
+    return function(req, res, next) {
+        req.users = usercreate;
+        next();
+    }
+}
