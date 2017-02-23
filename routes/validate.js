7b28408aaf2e4b1db66360a1791586e71d82a998
vaibhavpathak1994 <vaibhav_pathak@excellencetechnologies.in>
2017-02-23 18:37:22 +0530

    modified validate.js

--- routes/validate.js
+++ routes/validate.js
@@ -1,20 +1,20 @@
 var moment = require('moment');
 module.exports = function(req, res, next) {
-        var access_token = req.url.split("/");
-        var startDate=moment(access_token[3],"YYYY-MM-DD'T'HH:mm:ss:SSSZ");
-        var endDate = moment();
-        var Difference= endDate.diff(startDate);
-        if(Difference < 60*60*1000){
-            req.users.findOne({
-               _id: access_token[3]
-            },function(err, result) {
-            if (err) {
-                res.json('you are not authenticated');
-            }else {
-                next();
-            }
-            })
-        }else{
-        	res.json('you are not authenticated');
+    var access_token = req.url.split("/");
+    var startDate=moment(access_token[3],"YYYY-MM-DD'T'HH:mm:ss:SSSZ");
+    var endDate = moment();
+    var Difference= endDate.diff(startDate);
+    if(Difference < 60*60*1000){
+        req.users.findOne({
+          _id: access_token[3]
+        },function(err, result) {
+        if (err) {
+            res.json('you are not authenticated');
+        }else {
+            next();
         }
+        })
+    }else{
+      res.json('you are not authenticated');
+    }
 };
