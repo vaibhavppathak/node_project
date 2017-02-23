b34ecbf08f1648a022d5ad612d4ca784eb6f0113
saurabhketech <shekhar>
2017-02-23 14:08:06 +0530

    validation using middleware function

--- routes/validate.js
+++ routes/validate.js
@@ -1,10 +1,9 @@
-const path = require('path');
 module.exports = function(req, res, next) {
 	var token = req.url.split("/");
 	if(token[3]){
 		req.users.findOne({
 			_id: token[3]
-		}, function(err, res1) {
+		},function(err, res1) {
 			if (err) {
 				res.json('you are not authenticated');
 			} else if(!res1){
