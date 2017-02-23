9b9bd4c7e740b1d845ed046a51f496c970b709c2
saurabhketech <shekhar>
2017-02-23 12:51:34 +0530

    validation with accesstoken with middleware function

---
+++ routes/validate.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = function(req, res, next) {
+	var token = req.url.split("/");
+	if(token[3]){
+		req.users.findOne({
+			_id: token[3]
+		}, function(err, res1) {
+			if (err) {
+				res.json('you are not authenticated');
+			} else if(!res1){
+				res.json('you are not authenticated');
+			}else {
+				next();
+			}
+		})
+	}else{
+		next();
+	}
+};
