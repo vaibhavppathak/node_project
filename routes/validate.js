a960083e98728d92e36f60a0f72357bae40195b7
saurabhketech <shekhar>
2017-02-24 14:07:39 +0530

    validate with token

--- routes/validate.js
+++ routes/validate.js
@@ -1,20 +1,27 @@
 var path = require("path");
+var moment = require("moment");
 module.exports = function(req, res, next) {
     var token = req.url.split("/");
-    console.log(token[3])
     if (req.method != "POST") {
-        req.access_token.findOne({
-            userid: token[3]
-        }, function(err, res1) {
-            if (err) {
-                res.json('you are not authenticated');
-            } else if (!res1) {
-                res.json('you are not authenticated');
-            } else {
-                req.token = res1.userid;
-                next();
-            }
-        })
+        var startDate = moment(token[3], "YYYY-MM-DD'T'HH:mm:ss:SSSZ");
+        var endDate = moment();
+        var Difference = endDate.diff(startDate);
+        if (Difference < 60 * 60 * 1000) {
+            req.access_token.findOne({
+                token: token[3]
+            }, function(err, res1) {
+                if (err) {
+                    res.json('you are not authenticated');
+                } else if (!res1) {
+                    res.json('you are not authenticated');
+                } else {
+                    req.token = res1.userid;
+                    next();
+                }
+            })
+        } else {
+            res.json("not authenticated");
+        }
     } else {
         next();
     }
